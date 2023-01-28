5f134d835d443fc91912a4b54b8138fe68ef013a
Egemen TÜRK <turk.egemen@hotmail.com>
2023-01-28 10:10:20 +0300

    Initial Commit

---
+++ SwiftUICanvas/SwiftUICanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICanvasApp.swift
+//  SwiftUICanvas
+//
+//  Created by Egemen TÜRK on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
